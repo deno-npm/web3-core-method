d407a4f550cec60b0942b260f8ea1a09ae99b743
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-09-06 16:57:44 -0500

    Seemingly working port

---
+++ deps.js
@@ -0,0 +1,6 @@
+export * as _ from "https://cdn.skypack.dev/-/underscore@v1.11.0-8iEGGigleKa9YmBQ2bJF/dist=es2020/underscore.js";
+export * as EthersTransactionUtils from "https://cdn.skypack.dev/-/@ethersproject/transactions@v5.0.4-zFbMw47kcGejZqlo0WlP/dist=es2020/@ethersproject/transactions.js";
+export * as helpers from "https://denopkg.com/deno-npm/deno_npm/web3-core-helpers/v1.2.11/mod.js";
+export * as promiEvent from "https://denopkg.com/deno-npm/deno_npm/web3-core-promievent/v1.2.11/mod.js";
+export * as Subscriptions from "https://denopkg.com/deno-npm/web3-core-subscriptions/mod.js";
+export * as utils from "https://denopkg.com/deno-npm/deno_npm/web3-utils/v1.2.11/mod.js";
